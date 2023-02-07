3061ffea7b21e6037f9325aebcf5d57c9cce1502
NicolasRamirezPradaCampus <nicolascampus08@gmail.com>
2023-02-07 08:23:23 -0500

    ejercico5

---
+++ SKILLS REVIEW/ej5.py
@@ -0,0 +1,17 @@
+""" 5. Enumere 5 tipos de datos en Python y suministre un valor de
+ejemplo de cada uno. """
+
+x=input("digite un numero entero: \n")
+print ("dato entero " + x)
+
+q=input("digite cadena de caracteres: \n")
+print("cadena tipo string: "+q)
+
+w=input("digite numero decimal ")
+print("numero flotante: "+w)
+
+e=input("digite nombre \n")
+r=input("digite apellido \n ")
+print(f"cadena de datos lista [{e},{r}]")
+
+
