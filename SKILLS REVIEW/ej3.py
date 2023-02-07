1770fb478aa120eb5898d9614e8e78b5c5fd0db2
NicolasRamirezPradaCampus <nicolascampus08@gmail.com>
2023-02-07 07:50:04 -0500

    ejercicio 3 real

--- SKILLS REVIEW/ej3.py
+++ SKILLS REVIEW/ej3.py
@@ -1,1 +1,12 @@
-sdcsdcsdasxsac
+""" 3. Si hay operadores de igual precedencia, en qué orden se
+ejecutan?
+
+A. De izquierda a derecha
+B. De derecha a izquierda """
+
+num1=3
+num2=10
+num3=4
+num4=2
+num5=2
+print(f"a. de izquiera a derecha ({num1} *{num2} +{num4}) ({num3}*{num4} // {num5})({num1 *num2 +num4}) ({num3*num4 // num5}) ")
