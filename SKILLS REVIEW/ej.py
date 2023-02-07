525af349a145baff3b6b3a37416de42d5e4fa467
NicolasRamirezPradaCampus <nicolascampus08@gmail.com>
2023-02-07 07:25:22 -0500

    ej1

---
+++ SKILLS REVIEW/ej.py
@@ -0,0 +1,13 @@
+#EJ 1
+""" 1. Qué operadores utiliza Python en los siguientes casos:
+
+A. División Modular
+B. Exponenciación
+C. División que retorne entero. """
+
+num1=43
+num2=3
+
+print(f"exponenciacion {num1}**{num2} = {num1**num2}")
+print(f"division que retorne entero {num1}//{num2} = {num1//num2}")
+print(f"division modular {num1}%{num2} = {num1%num2}")
