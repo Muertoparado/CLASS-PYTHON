212a55a1dac8b0f2c38de0aaa24848f7c9aaaf3c
NicolasRamirezPradaCampus <nicolascampus08@gmail.com>
2023-02-07 09:25:38 -0500

    ej8

---
+++ SKILLS REVIEW/ej8.py
@@ -0,0 +1,27 @@
+""" 8. Escriba un bloque cualquiera de código en Python en donde
+utilice 2 condicionales (if) anidados. """
+
+print("-------BIENVENIDO--------\n")
+c=print("desea ingresar a la tienda  [s/n]")
+
+if c=='s':
+        print("productos \n")
+        print("1.camisas \n")
+        print("2.zapatos \n")
+        print("3.carteras \n")
+
+        p=input("digite el numero de la lista del producto a seleccionar")
+        if(p==1):
+            print("a seleccionado camisas")
+        elif(p==2):
+            print("a seleccionado zapatos")
+        elif(p==3):
+            print("a seleccionado cartera")
+
+        elif(c=='n'):
+            print("pronto regreso")
+
+
+
+else:
+            print("asd-")
