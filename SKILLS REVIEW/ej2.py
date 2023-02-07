f288b8ed0ae10c560d638e7c6f7f9b76c4edb318
NicolasRamirezPradaCampus <nicolascampus08@gmail.com>
2023-02-07 07:35:13 -0500

    ejercicio 2

---
+++ SKILLS REVIEW/ej2.py
@@ -0,0 +1,6 @@
+""" 2. En la jerarquía de operadores, cuáles son los que más
+prioridad tienen cuando el intérprete de Python los evalúa? """
+
+
+print("prioridad 1()""\n 2.potencia ** \n 3. (Multiplicación y División, módulo o residuo,División entera)")
+print("4. suma resta \n 5.operadores relacionales \n 6. operadores logicos ")
