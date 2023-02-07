22b0982682bd51322c382635501b5f99055eefcd
NicolasRamirezPradaCampus <nicolascampus08@gmail.com>
2023-02-07 08:57:32 -0500

    ej6

---
+++ SKILLS REVIEW/ej6.py
@@ -0,0 +1,10 @@
+""" 6. En sus propias palabras, qué son las funciones
+preconstruidas y proporcione 2 ejemplos. """
+
+#son caracteristicas las cuales modifican o permite trabajar mas adecuadamente los datos
+
+li=[2,4,6,2,6,3,7]
+print(f"{li} caracteres totales en la lista: len ", len(li))
+
+txt="qweerty ksjdkksl"
+print(f" {txt} \n modificando el texto func upper: \n", txt.upper())
