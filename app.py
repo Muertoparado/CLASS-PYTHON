ab0858fe63a33d4c88d5957d7efdcf125f840538
NicolasRamirezPradaCampus <nicolascampus08@gmail.com>
2023-02-07 07:24:02 -0500

    EJ1

---
+++ app.py
@@ -0,0 +1,36 @@
+nombre= "asddfsdf"
+edad =24
+estatura=1.45
+soyprofesor =False
+#lista
+pasatiempo=["programar", "qwdwew","acsdcsd"]
+
+#diccionario
+listaropa= {
+    "gorra": False,
+    "pantalon": "azul",
+    
+}
+#print(listaropa.get("gorra"))
+
+#lista inmutable
+
+listaInmutable= tuple("HOLAS")
+#print ("".join(list(listaInmutable)))
+
+
+#vacio
+vacio=None
+##print(vacio)
+
+noNumero=float("nan")
+#print(noNumero)
+
+
+#interpolar
+#print(f'\t {nombre}')
+
+x=input('ingresar num \n')
+print (x)
+
+
