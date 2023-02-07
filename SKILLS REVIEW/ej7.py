618a33a26815b0f51395e923002f0cc4cba4e2d8
NicolasRamirezPradaCampus <nicolascampus08@gmail.com>
2023-02-07 09:06:08 -0500

    ej7

---
+++ SKILLS REVIEW/ej7.py
@@ -0,0 +1,5 @@
+""" 7. Cuál es la diferencia entre un condicional simple y un
+condicional compuesto? """
+
+#condicional simple solo evalua si se cumple su funcion sino sigue el codigo normal 
+#condicional compuesto puede abarcar if anidados de tal forma que evalue distintas condiciones tenindo en cuenta si se cumple la funcion o no 
