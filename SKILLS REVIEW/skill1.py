41b03f58311c0980b9de76ac0964e24d3cfeff21
NicolasRamirezPradaCampus <nicolascampus08@gmail.com>
2023-02-17 09:18:48 -0500

    ej1 medio

--- SKILLS REVIEW/skill1.py
+++ SKILLS REVIEW/skill1.py
@@ -1,46 +1,51 @@
 def crear():
-    if(g=='1'):
+    if(g==1):
+        global artemis
         artemis=[]
-    if(g=='2'):
+        print("lista creada")
+    if(g==2):
+        global sputnik
         sputnik=[]
+        print("lista creada")
 
 def lista():
-    if(g=='1.1'):
+    if(g==1.1):
        print(artemis)
-    if(g=='2.1'):
+    if(g==2.1):
        print(sputnik)
-
+nombre=""
 def agregar():
-    if(g=='1.2'):
-        nombre=input("Agregar nombre")
+    
+    if(g==1.2):
+        nombre=input("Agregar nombre: ")
         artemis.append(nombre)
-        print("Usuario Artemis agregado")
-    if(g=='2.2'):
+        print("Usuario Artemis agregado. ")
+    if(g==2.2):
         nombre=input("Agregar nombre")
         sputnik.append(nombre)
         print("Usuario Sputnik agregado")
 
 def eliminar():
-    if(g=='1.3'):
+    if(g==1.3):
         nombre=input("Agregar nombre")
         artemis.remove(nombre)
-        print("Usuario Artemis agregado")
-    if(g=='2.3'):
+        print("Usuario Artemis eliminado")
+    if(g==2.3):
         nombre=input("Agregar nombre")
         sputnik.remove(nombre)
-        print("Usuario Sputnik agregado")
+        print("Usuario Sputnik eliminado")
 
 def ordenar():
-    if(g=='1.4'):
-        sorted(artemis.split())
+    if(g==1.4):
+        artemis.sorted(artemis)
         print(f"Lista ordenada Artemis {artemis}")
-    if(g=='2.4'):
+    if(g==2.4):
         sorted(sputnik.split())
         print(f"Lista ordenada Sputnik {sputnik}")
 
 def buscar():
     nom=input("digite el nombre a buscar")
-    if(g=='1.5'):
+    if(g==1.5):
         if(nom in artemis):
             print(f"usuario encontrado {nom}")
     if(g==2.5):
@@ -49,7 +54,8 @@ def buscar():
 
 q=1     
 while(q!=0):
-    g=input("----------MENU-----------\n"
+    
+    g=float(input("----------MENU-----------\n"
     "1. Crear lista Artemis\n"
     "1.1 Listar camper Artemis\n"
     "1.2 Agregar camper Artemis\n"
@@ -61,45 +67,49 @@ while(q!=0):
     "2.2 Agregar camper Sputnik\n"
     "2.3 Eliminar camper de Sputnik\n"
     "2.4 Ordenar lista Sputnik\n"
-    "2.5 Buscar camper lista Sputnik\n")
-    if(g=='1'):
-        print("A seleccionado Crear lista Artemis ")
-        crear()
-    if(g=='1.1'):
-        print("A seleccionado Listar camper Artemis ")
-        lista()
-    if(g=='1.2'):
-        print("A seleccionado Agregar camper Artemis ")
-        agregar()
-    if(g=='1.3'):
-        print("A seleccionado Eliminar camper de artemis")
-        eliminar()
-    if(g=='1.4'):
-        print("A seleccionado Ordenar lista Artemis")
-        ordenar()
-    if(g=='1.5'):
-        print("A seleccionado Buscar camper lista Artemis")
-        ordenar()
+    "2.5 Buscar camper lista Sputnik\n"
+    "0. salir\n"))
+    
+    if(g==1):
+            print("A seleccionado Crear lista Artemis ")
+            crear()
+    elif(g==1.1):
+            print("A seleccionado Listar camper Artemis ")
+            lista()
+    elif(g==1.2):
+            print("A seleccionado Agregar camper Artemis ")
+            agregar()
+    elif(g==1.3):
+            print("A seleccionado Eliminar camper de artemis")
+            eliminar()
+    elif(g==1.4):
+            print("A seleccionado Ordenar lista Artemis")
+            ordenar()
+    elif(g==1.5):
+            print("A seleccionado Buscar camper lista Artemis")
+            ordenar()
 
-    if(g=='2'):
-        print("A seleccionado Crear lista Sputnik ")
-        crear()
-    if(g=='2.1'):
-        print("A seleccionado Listar camper Sputnik")
-        lista()
-    if(g=='2.2'):
-        print("A seleccionado Agregar camper Sputnik ")
-        agregar()
-    if(g=='2.3'):
-        print("A seleccionado Eliminar camper de Sputnik")
-        eliminar()
-    if(g=='2.4'):
-        print("A seleccionado Ordenar lista Sputnik")
-        ordenar()
-    if(g=='2.5'):
-        g=2.5
-        print("A seleccionado Buscar camper lista Sputnik")
-        ordenar()
+    elif(g==2):
+            print("A seleccionado Crear lista Sputnik ")
+            crear()
+    elif(g==2.1):
+            print("A seleccionado Listar camper Sputnik")
+            lista()
+    elif(g==2.2):
+            print("A seleccionado Agregar camper Sputnik ")
+            agregar()
+    elif(g==2.3):
+            print("A seleccionado Eliminar camper de Sputnik")
+            eliminar()
+    elif(g==2.4):
+            print("A seleccionado Ordenar lista Sputnik")
+            ordenar()
+    elif(g==2.5):
+            g=2.5
+            print("A seleccionado Buscar camper lista Sputnik")
+            ordenar()
+            
+    
 
 
 
