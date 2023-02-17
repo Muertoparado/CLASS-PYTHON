0ab2c8d322dc3e82aa4998244f8a52886f6f4e96
NicolasRamirezPradaCampus <nicolascampus08@gmail.com>
2023-02-17 13:46:50 -0500

    4 medio

---
+++ SKILLS REVIEW/skill4.py
@@ -0,0 +1,25 @@
+""" 4. Una empresa tiene 500 almacenes. Cada almacén debe
+reportar el nombre y 5 registros c/u, contiene el tipo de articulo
+y el número de unidades vendidas de ese artículo.
+
+Haga un programa en Python para determinar cuál fue el
+almacén que mas vendió, cual fue el articulo más vendido de
+ese almacén y cual el más vendido en general. """
+
+""" https://lineadecodigo.com/python/lista-aleatoria-python/ """
+a1=1000
+a2=2000
+a3=3000
+a4=4000
+a5=5000
+a6=6000
+a7=7000
+a8=8000
+
+
+tienda1={"a1":15,"a2":1,"a3":0,"a4":70,"a5":50}
+tienda2={"a1":200,"a2":14,"a3":3,"a4":20,"a5":10}
+tienda3={"a1":170,"a2":10,"a3":10,"a4":600,"a5":30}
+tienda4={"a1":1000,"a2":1400,"a3":3000,"a4":700,"a5":5000}
+tienda5={"a1":1000,"a2":1400,"a3":3000,"a4":700,"a5":5000}
+
